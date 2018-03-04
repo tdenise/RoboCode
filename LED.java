ef02c7925bf620634d356249f093b656bbeb5da3
Denise Tran <tdenise@users.noreply.github.com>
2018-03-03 17:56:26 -0800

    Add files via upload

--- LED.java
+++ LED.java
@@ -33,8 +33,6 @@ public class LED extends Robot
 		check = false;
 		
 		// turnLeft to face a wall.
-		// getHeading() % 90 means the remainder of
-		// getHeading() divided by 90.
 		turnLeft(getHeading() % 90);
 		ahead(moveAmount);
 		// Turn the gun to turn right 90 degrees.
@@ -46,25 +44,12 @@ public class LED extends Robot
 			// Look before we turn when ahead() completes.
 			check = true;			
 			// Turn to the next wall
-			turnRight(90);
 			ahead(moveAmount);
 			// Don't look now
 			check = false;
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
-		}	
-		
-/*		// Robot main loop
-		while(true) {
-			// Replace the next 4 lines with any behavior you would like
-			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
-		}*/
-	}
-	
+			turnRight(90);
+		}
+	}		
 
 	/**
 	 * onScannedRobot: What to do when you see another robot
@@ -72,35 +57,33 @@ public class LED extends Robot
 	public void onScannedRobot(ScannedRobotEvent e) {
 		double absoluteBearing = getHeading() + e.getBearing();
 		double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
-		
-		if (Math.abs(bearingFromGun) <= 3) {
-			turnGunRight(bearingFromGun);
-			// We check gun heat here, because calling fire()
-			// uses a turn, which could cause us to lose track
-			// of the other robot.
-			if (getGunHeat() == 0) {
-				fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+		if(check){
+			if (Math.abs(bearingFromGun) <= 3) {
+				turnGunRight(bearingFromGun);
+				// We check gun heat 
+				if (getGunHeat() == 0) {
+					fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+				}
+			} // otherwise just set the gun to turn.
+			// Note:  This will have no effect until we call scan()
+			else {
+				turnGunRight(bearingFromGun);
 			}
-		} // otherwise just set the gun to turn.
-		// Note:  This will have no effect until we call scan()
-		else {
-			turnGunRight(bearingFromGun);
-		}
-		
-			fire(10);
-			if(check){
+			
+			if(bearingFromGun == 0) { 
 				scan();
 			}
+		}
 	}
 	
 
 
-	public void onGotHit(HitByBulletEvent e) {
+	public void onHitByBullet(HitByBulletEvent e) {
+		moveAmount = Math.max(getBattleFieldWidth(), getBattleFieldHeight());
 		if(e.getBearing() > -90 && e.getBearing() < 90) {
-			turnRight(90);
-			ahead(100);
-			back(30);
+			ahead(moveAmount);
 		}
+	
 	}	
 
 	//move back a bit if robo in front of us
